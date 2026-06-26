befeebba06ba759191d953e97e7fe9f8978f142c
junior <seu-email@exemplo.com>
2026-06-26 07:26:52 -0300

    Trabalhando as funções

--- scripts.js
+++ scripts.js
@@ -1,11 +1,19 @@
+//Cotação de moeda do dia
+const USD = 4.87
+const EUR = 5.32
+const GBP = 6.08
+
+
 const form = document.querySelector("form")
 const amount = document.getElementById ("amount")
 const currency = document.getElementById("currency")
+const footer = document.querySelector("main footer")
+const description = document.getElementById ("description")
 
 // Manipulando o input para receber apenas números
 amount.addEventListener ("input", () => {
 
-  console.log(amount.value)
+  // console.log(amount.value)
   const hasCharactersRegex = /\D+/g
   amount.value = amount.value.replace(hasCharactersRegex, "")
 })
@@ -13,6 +21,34 @@ amount.addEventListener ("input", () => {
 form.onsubmit = (event) => {
   event.preventDefault ()
 
-  console.log(currency.value)
+  switch (currency.value) {
+    case "USD": 
+      convertCurrency(amount.value, USD, "US$")
+      break
+    case "EUR":
+      convertCurrency(amount.value, EUR, "€")
+      break
+    case "GBP":
+      convertCurrency(amount.value, GBP, "£")
+      break
+  }
+
+}
+
+// Função para converter a moeda 
+
+function convertCurrency (amount, price, symbol) {
+ try {
+
+  description.textContent = `${symbol} 1 = ${price}`
+//Aplica a classe que mostra o footer
+  footer.classList.add("show-result")
+  
+ } catch (error) {
+  footer.classList.remove("show-result")
+  alert("Não foi possível converter a moeda, tente novamente mais tarde.")
+
+  
+ }
 
 }

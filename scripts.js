09722477d96e3b1428c154b1f76ff59125392047
junior <seu-email@exemplo.com>
2026-06-27 07:14:10 -0300

    Finalizando o projeto

--- scripts.js
+++ scripts.js
@@ -5,13 +5,14 @@ const GBP = 6.08
 
 
 const form = document.querySelector("form")
-const amount = document.getElementById ("amount")
+const amount = document.getElementById("amount")
 const currency = document.getElementById("currency")
 const footer = document.querySelector("main footer")
-const description = document.getElementById ("description")
+const description = document.getElementById("description")
+const result = document.getElementById("result")
 
 // Manipulando o input para receber apenas números
-amount.addEventListener ("input", () => {
+amount.addEventListener("input", () => {
 
   // console.log(amount.value)
   const hasCharactersRegex = /\D+/g
@@ -19,10 +20,10 @@ amount.addEventListener ("input", () => {
 })
 
 form.onsubmit = (event) => {
-  event.preventDefault ()
+  event.preventDefault()
 
   switch (currency.value) {
-    case "USD": 
+    case "USD":
       convertCurrency(amount.value, USD, "US$")
       break
     case "EUR":
@@ -37,18 +38,42 @@ form.onsubmit = (event) => {
 
 // Função para converter a moeda 
 
-function convertCurrency (amount, price, symbol) {
- try {
+function convertCurrency(amount, price, symbol) {
+  try {
+    // Exibe o resultado da conversão no footer
+    description.textContent = `${symbol} 1 = ${formatCurrencyBRL(price)}`
 
-  description.textContent = `${symbol} 1 = ${price}`
-//Aplica a classe que mostra o footer
-  footer.classList.add("show-result")
-  
- } catch (error) {
-  footer.classList.remove("show-result")
-  alert("Não foi possível converter a moeda, tente novamente mais tarde.")
+    // Calcula o total
+    let total = amount * price
 
-  
- }
+    // Verifica se o resultado não é um número
+    if (isNaN(total)) {
+      return alert ("Por favor, digite o valor corretamente.")
+    }
+
+    //Formatando o valor total
+    total = formatCurrencyBRL(total).replace("R$", "")
+
+    // Exibe o valor
+    result.textContent = `${total} Reais`
+
+
+    //Aplica a classe que mostra o footer
+    footer.classList.add("show-result")
+
+  } catch (error) {
+    footer.classList.remove("show-result")
+    alert("Não foi possível converter a moeda, tente novamente mais tarde.")
+
+  }
+}
+// Função para formatar o valor em moeda brasileira
+
+function formatCurrencyBRL(value) {
+  return Number(value).toLocaleString("pt-br", {
+    style: "currency",
+    currency: "BRL"
+  })
+
 
 }
